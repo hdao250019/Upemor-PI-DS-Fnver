98fc87ea9af09de36928f2d413f9c732475c97a5
Jared <Jared@172.23.18.177>
2026-07-03 09:28:36 -0600

    sin links comentarios

---
+++ src/Controller/UserController.java
@@ -0,0 +1,154 @@
+package Controller;
+
+import MainForms.FrmConsultarUsuarios;
+import MainForms.FrmNuevoUsuario;
+import Models.UsuarioBD;
+import Models.Usuario;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Jared
+ */
+public class UserController implements ActionListener{
+    
+    //crear objetos para el modelo y la vista
+    private FrmNuevoUsuario ventana;
+    private UsuarioBD usuariobd;
+    private FrmConsultarUsuarios verUsuarios;
+    
+    // Constructor para iniciazar los ojetos}
+    public UserController(FrmNuevoUsuario ventana, UsuarioBD usuariobd,FrmConsultarUsuarios verUsuarios) {
+        this.ventana = ventana;
+        this.usuariobd = usuariobd;
+        this.verUsuarios = verUsuarios;
+        
+        // Verifica si el objeto de la ventana de registro existe
+        if (this.ventana != null) {
+            // Verifica si el botón Crear Cuenta ya fue inicializado en los componentes
+            if (this.ventana.BTNCrear != null) {
+                this.ventana.BTNCrear.addActionListener(this);
+            }
+        }
+        // Verifica si el objeto de la ventana de consulta de usuarios existe
+        if (this.verUsuarios != null) {
+            // Verifica si el botón Mostrar Usuarios existe en el frame
+            if (this.verUsuarios.btnUsuarios != null) {
+                this.verUsuarios.btnUsuarios.addActionListener(this);
+            }
+            mostrarUsuarios();
+        }
+    }
+    
+    //Metodo obligatorio para la clase abstracta
+    //Override es sobreescribir el código de un metodo existente (actionPerformed)
+    @Override
+    public void actionPerformed(ActionEvent e){
+        
+    // Validamos que ventana exista antes de preguntar por su botón
+    // Sin esta validacion java no encuentra la ventana por lo que marca error
+        if (this.ventana != null && this.ventana.BTNCrear != null) {
+            if (e.getSource() == this.ventana.BTNCrear) {
+                
+                guardarUsuario();
+            }
+        }
+
+        // Validamos que verUsuarios exista antes de preguntar por su botón
+        if (this.verUsuarios != null && this.verUsuarios.btnUsuarios != null) {
+            if (e.getSource() == this.verUsuarios.btnUsuarios) {
+                mostrarUsuarios();
+            }
+        }
+}
+    
+    // Metodo para guardar estudiantes
+    private void guardarUsuario(){
+        
+        // Obtener los datos desde la ventana
+        String nombre = ventana.txtNombre.getText().trim();
+        String correo = ventana.txtCorreo.getText().trim();
+        int edad = Integer.parseInt(ventana.spnEdad.getValue().toString());
+        String contraseña = ventana.txtPass.getText().trim();
+        
+        if (nombre.isEmpty() || correo.isEmpty() || contraseña.isEmpty() || edad <= 0) {
+            // Mostrar un mensaje de advertencia al usuario
+            JOptionPane.showMessageDialog(ventana, "Por favor, complete todos los campos.", 
+                    "Campos Incompletos", JOptionPane.WARNING_MESSAGE);
+        }
+        
+        
+        Usuario user = new Usuario(edad, nombre, correo, contraseña);
+        // Enviar el objeto de usuario antes de guardado para que el modelo lo reciba
+        boolean resultado = usuariobd.insertar(user);
+        
+        // Evaluar si el resultado de la insercion fue exitoso
+        if (resultado) {
+            JOptionPane.showMessageDialog(ventana, "Registro exitoso");
+            LimpiarCampos();
+            
+            // Solo actualiza la tabla si la ventana existe
+            if (this.verUsuarios != null) {
+                mostrarUsuarios();
+            }
+        } else {
+            JOptionPane.showMessageDialog(ventana, "Error al registrar");
+        }
+    }
+    
+    
+    
+    // Metodo para crear la tabla
+    private void mostrarUsuarios(){
+    
+        DefaultTableModel modelo = new DefaultTableModel();
+        
+        // Añadir columnas de la tabla
+        modelo.addColumn("ID");
+        modelo.addColumn("Nombre");
+        modelo.addColumn("Edad");
+        modelo.addColumn("Correo");
+        modelo.addColumn("Contraseña");
+        
+        List<Usuario> listaUsuarios = usuariobd.consultarUsuarios();
+        
+        // Ciclo para recorrer la lista de usuarios
+        for(Usuario usuario : listaUsuarios){
+            
+            // Guardar los objetos en un arreglo para usarlos en la tabla
+            Object[] fila = {
+                usuario.getId(),
+                usuario.getNombre(),
+                usuario.getEdad(),
+                usuario.getCorreo(),
+                usuario.getContrasenia()
+                    
+            };
+            
+            modelo.addRow(fila);
+        }
+        //Crear tabla
+        JTable tablaUser = new JTable(modelo);
+        // Usar JScrollpane para mostrar la tabla
+        verUsuarios.paneUsuarios.setViewportView(tablaUser);
+        
+    }
+        
+    
+    
+    
+    private void LimpiarCampos(){
+        ventana.txtNombre.setText("");
+        ventana.txtCorreo.setText("");
+        ventana.spnEdad.setValue(0);
+        ventana.txtCorreo.setText("");
+        ventana.txtPass.setText("");
+        
+        
+    }
+}
