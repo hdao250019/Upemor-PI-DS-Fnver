5a7db099a2d8a87c7229105e580f492326244bb3
Jared <Jared@LAPTOP-LPHP09>
2026-07-06 10:57:26 -0600

    04/07/2026 ventanas

--- src/finver/Main.java
+++ src/finver/Main.java
@@ -1,5 +1,6 @@
 package finver;
 
+import Controller.MenuController;
 import Controller.UserController;
 import Views.FrmConsultarUsuarios;
 import Views.FrmNuevoUsuario;
@@ -7,11 +8,14 @@ import Views.SplashFrm;
 import Models.UsuarioBD;
 import Views.FrmConsultarCategorias;
 import Views.FrmIngresarCategoria;
+import Views.MainFrm;
 
 /**
  *
  * @author rousc
  */
+
+
 public class Main {
      public static void main(String[] args) {
         FrmNuevoUsuario ventana = new FrmNuevoUsuario();
@@ -20,9 +24,12 @@ public class Main {
         FrmConsultarUsuarios verUsuarios = new FrmConsultarUsuarios();
         FrmIngresarCategoria ingresarCategoria = new FrmIngresarCategoria();
         FrmConsultarCategorias consultarCat = new FrmConsultarCategorias();
-
-        
+        MainFrm vistaMenu = new MainFrm();
+        UsuarioBD modeloBD = new UsuarioBD();
+    
+        MenuController controlMenu = new MenuController(vistaMenu, modeloBD);
         UserController controlador = new UserController(ventana, usuariobd, verUsuarios, ingresarCategoria, consultarCat);
+        
         inicio.setLocationRelativeTo(null);
         inicio.setVisible(true);
     }
